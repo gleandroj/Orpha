06bd6c20d2641c8b3e074a6bdc57dd736e58ba3b
Gabriel <gabrielleandrojunior@live.com>
2017-02-22 16:42:00 -0300

    Fix laravel modules route path include

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: sh app_boot.sh
-worker: php artisan queue:listen
+vendor/bin/heroku-php-apache2 public
--- app_boot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Artisan commands
-/app/.heroku/php/bin/php /app/artisan clear-compiled
-/app/.heroku/php/bin/php /app/artisan optimize
-
-# Boot up!
-vendor/bin/heroku-php-nginx -C nginx_app.conf public/
