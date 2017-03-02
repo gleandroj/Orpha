949fd2e60795f6a28f3287cfed36d0736302d9eb
Gabriel <gabrielleandrojunior@live.com>
2017-03-02 09:23:23 -0300

    Fix bug's

--- resources/assets/js/config/AngularConfig.js
+++ resources/assets/js/config/AngularConfig.js
@@ -1,7 +1,9 @@
 
-AngularConfig.$inject = ['$qProvider'];
+AngularConfig.$inject = ['$qProvider', '$locationProvider'];
 
-export default function AngularConfig($qProvider){
+export default function AngularConfig($qProvider, $locationProvider){
      //Default = False for production Environment, for debug use TRUE, to Prevent show Transition error on redirect user
     $qProvider.errorOnUnhandledRejections(true);
+
+    $locationProvider.html5Mode(true).hashPrefix('!');
 }
