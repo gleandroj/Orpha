db5f90209a48177850a56c00dd510f409e5df056
Gabriel <gabrielleandrojunior@live.com>
2017-03-02 10:56:12 -0300

    Remove html 5 url mode

--- resources/assets/js/config/AngularConfig.js
+++ resources/assets/js/config/AngularConfig.js
@@ -3,9 +3,4 @@ AngularConfig.$inject = ['$qProvider', '$locationProvider'];
 export default function AngularConfig($qProvider, $locationProvider) {
     //Default = False for production Environment, for debug use TRUE, to Prevent show Transition error on redirect user
     $qProvider.errorOnUnhandledRejections(true);
-
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    }).hashPrefix('!');
 }
