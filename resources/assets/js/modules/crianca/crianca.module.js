31ff1e42caf955e05cf825ce835288be9ebb358e
Gabriel <gabrielleandrojunior@live.com>
2017-03-15 11:39:29 -0300

    Add Crianca Form Component

--- resources/assets/js/modules/crianca/crianca.module.js
+++ resources/assets/js/modules/crianca/crianca.module.js
@@ -6,10 +6,12 @@ import RouteConfig from './config/RouteConfig';
 import CriancaService from './services/CriancaService';
 
 import { CriancaListComponent } from './components/CriancaListComponent/CriancaListComponent';
+import { CriancaFormComponent } from './components/CriancaFormComponent/CriancaFormComponent';
 
 let module = angular
     .module('orpha.modules.crianca', [])
     .component(CriancaListComponent.selector, CriancaListComponent)
+    .component(CriancaFormComponent.selector, CriancaFormComponent)
     .service('CriancaService', CriancaService)
     .config(RouteConfig);
 
