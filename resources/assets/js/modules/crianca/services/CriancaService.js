5345033e1878c5a6b60dde45c09166cdaac2ffcc
Gabriel <gabrielleandrojunior@live.com>
2017-03-15 11:40:14 -0300

    Add Crianca Model

--- resources/assets/js/modules/crianca/services/CriancaService.js
+++ resources/assets/js/modules/crianca/services/CriancaService.js
@@ -2,6 +2,8 @@
  * Created by FG0003 on 09/02/2017.
  */
 
+class Crianca {}
+
 export default class CriancaService{
 
     constructor($http, OrphaUtilService) {
@@ -14,7 +16,12 @@ export default class CriancaService{
     getAll() {
         let $q = this._util.defer();
 
-        this._http.get(this._url).then((response)=> $q.resolve(response.data), (response) => $q.reject(response.data));
+        this._http.get(this._url)
+        .then((response)=> {
+            let items = response.data;
+            items.map((crianca) => this._util.extend(new Crianca(), crianca));
+            $q.resolve(items);
+        }, (response) => $q.reject(response.data));
 
         return $q.promise;
     }
@@ -22,7 +29,7 @@ export default class CriancaService{
     get(id) {
         let $q = this._util.defer();
 
-        this._http.get(this._url + '/' + id).then((response)=> $q.resolve(response.data), (response) => $q.reject(response.data));
+        this._http.get(this._url + '/' + id).then((response)=> $q.resolve(this._util.extend(new Crianca(), response.data)), (response) => $q.reject(response.data));
 
         return $q.promise;
     }
@@ -31,9 +38,9 @@ export default class CriancaService{
         let defer = this._util.defer();
 
         if (user.id == null || user.id == '') {
-            this._http.post(this._url, user).then((response)=> defer.resolve(response.data), (response)=> defer.reject(response.data));
+            this._http.post(this._url, user).then((response)=> defer.resolve(this._util.extend(new Crianca(), response.data)), (response)=> defer.reject(response.data));
         } else {
-            this._http.put(this._url + '/' + user.id, user).then((response)=> defer.resolve(response.data), (response)=> defer.reject(response.data));
+            this._http.put(this._url + '/' + user.id, user).then((response)=> defer.resolve(this._util.extend(new Crianca(), response.data)), (response)=> defer.reject(response.data));
         }
         return defer.promise;
     }
@@ -41,7 +48,7 @@ export default class CriancaService{
     disable(user) {
         let $q = this._util.defer();
 
-        this._http.delete(this._url + '/' + user.id).then((response)=> $q.resolve(response.data), (response) => $q.reject(response.data));
+        this._http.delete(this._url + '/' + user.id).then((response)=> $q.resolve(this._util.extend(new Crianca(), response.data)), (response) => $q.reject(response.data));
 
         return $q.promise;
     }
@@ -49,7 +56,7 @@ export default class CriancaService{
     enable(user) {
         let $q = this._util.defer();
 
-        this._http.get(this._url + '/restore/' + user.id).then((response)=> $q.resolve(response.data), (response) => $q.reject(response.data));
+        this._http.get(this._url + '/restore/' + user.id).then((response)=> $q.resolve(this._util.extend(new Crianca(), response.data)), (response) => $q.reject(response.data));
 
         return $q.promise;
     }
