c0db46b5b70e6203b55137f1c4b99f67cfc8aff7
Gabriel <gabrielleandrojunior@live.com>
2017-03-17 16:30:40 -0300

    Fix controller namespace

--- app/Modulos/Orpha/routes/api.php
+++ app/Modulos/Orpha/routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth:api']], function () {
-    Route::get('orpha/dashboard', 'DashboardController@getData');
+    Route::get('orpha/dashboard', 'Controllers\DashboardController@getData');
 });
