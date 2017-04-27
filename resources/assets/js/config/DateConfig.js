c3c8a99004e54bca211b9719a8ce959374fe28e7
Gabriel <gabrielleandrojunior@live.com>
2017-04-27 16:49:05 -0300

    remove Provide Decorator mdDatePickerDir

--- resources/assets/js/config/DateConfig.js
+++ resources/assets/js/config/DateConfig.js
@@ -21,29 +21,4 @@ export default function DateConfig($mdDateLocaleProvider, $provide) {
     $mdDateLocaleProvider.days = ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'];
     $mdDateLocaleProvider.shortDays = ['D','S','T','Q','Q','S','S'];
 
-    $provide.decorator('mdDatepickerDirective', ['$delegate',
-        function ($delegate) {
-            let directive = $delegate[0];
-
-            let template = directive.template;
-
-            directive.template = function (tElement, tAttrs) {
-
-                var originalTemplate = template.apply(this, arguments);
-
-                var element = angular.element(originalTemplate);
-                element.find('input').attr('mask', '39/19/9999');
-                element.find('input').attr('restrict', 'reject');
-                element.find('input').attr('clean', 'true');
-                element.find('input').attr('ng-model', "ctrl.dateInput");//ng-model is required by ngMask
-                let newTemplate = '';
-                for(let i = 0; i < element.length ; i++)
-                    newTemplate += element[i].outerHTML;
-
-                return newTemplate;
-            };
-
-            return $delegate;
-        }
-    ]);
 }
