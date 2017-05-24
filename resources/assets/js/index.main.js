4dddb2cd834e884060c27fd9afc81631d168dbc2
Gabriel <gabrielleandrojunior@live.com>
2017-05-23 23:08:35 -0300

    Add md-steppers

--- resources/assets/js/index.main.js
+++ resources/assets/js/index.main.js
@@ -12,12 +12,15 @@ import 'angular-material-expansion-panel';
 import 'ng-mask/dist/ngMask.js';
 import 'chart.js';
 import 'angular-chart.js';
+import 'md-steppers';
 
 import 'angular-material-simple-components';
 import 'angular-material/angular-material.css';
 import 'angular-material-expansion-panel/dist/md-expansion-panel.min.css';
+import 'md-steppers/dist/md-steppers.min.css';
+
 //import './../css/animations.scss';
-//import './../css/style.scss';
+import './../css/style.scss';
 
 import AuthModule from './modules/auth/auth.module';
 import OrphaModule from './modules/orpha/orpha.module';
@@ -40,6 +43,7 @@ angular.module('orpha.modules', [
     'ui.router',
     'ngMask',
     'chart.js',
+    'md-steppers',
     AuthModule,
     OrphaModule,
     UserModule,
