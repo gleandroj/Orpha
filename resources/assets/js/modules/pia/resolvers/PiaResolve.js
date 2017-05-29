3fcbd73eaa7b506db05eb1ecb5a1422038cbd0f4
Gabriel <gabrielleandrojunior@live.com>
2017-05-29 20:27:28 -0300

    Fix build error

--- resources/assets/js/modules/pia/resolvers/PiaResolve.js
+++ resources/assets/js/modules/pia/resolvers/PiaResolve.js
@@ -2,8 +2,19 @@
  * Created by hc on 5/29/17.
  */
 
-export default function PiaResolve(PiaService) {
-    return {};
+export default function PiaResolve(PiaService, OrphaUtilService, $state, $stateParams) {
+    let deferred = OrphaUtilService.defer();
+    PiaService.get($stateParams.id)
+        .success((pia) => { deferred.resolve(pia) })
+        .error((error) => {
+            try{
+                $state.go('^');
+            }catch (err){
+                $state.go('crianca.list');
+            }
+            deferred.reject(error);
+        });
+    return deferred.promise;
 }
 
-PiaResolve.$inject = ['PiaService'];
+PiaResolve.$inject = ['PiaService', 'OrphaUtilService', '$state', '$stateParams'];
