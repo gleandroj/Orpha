a73f4a0e4e386725ed5b2ac979477baef48e11fb
Gabriel <gabrielleandrojunior@live.com>
2017-05-30 12:29:08 -0300

    Add default columns values to prevent vizualization state error

--- database/migrations/2017_05_29_113211_create_dadosenecessidades_table.php
+++ database/migrations/2017_05_29_113211_create_dadosenecessidades_table.php
@@ -22,46 +22,46 @@ class CreateDadosenecessidadesTable extends Migration
             $table->integer('pia_id')->unsigned();
             $table->foreign('pia_id')->references('id')->on('pia');
 
-            $table->boolean("documentacao_certidao_nascimento")->nullable();
-            $table->boolean("documentacao_historico_escolar")->nullable();
-            $table->boolean("documentacao_declaracao_escolar")->nullable();
+            $table->boolean("documentacao_certidao_nascimento")->default(0)->nullable();
+            $table->boolean("documentacao_historico_escolar")->default(0)->nullable();
+            $table->boolean("documentacao_declaracao_escolar")->default(0)->nullable();
             $table->string("documentacao_rg")->nullable();
             $table->string("documentacao_cpf")->nullable();
             $table->string("documentacao_titulo_eleitoral")->nullable();
             $table->string("documentacao_ctps")->nullable();
             $table->string("documentacao_reservista")->nullable();
             $table->string("documentacao_outros")->nullable();
-            $table->boolean("documentacao_completado")->nullable();
+            $table->boolean("documentacao_completado")->default(0)->nullable();
 
-            $table->boolean("necessidades_enfermagem")->nullable();
-            $table->boolean("necessidades_medico")->nullable();
-            $table->boolean("necessidades_nutricao")->nullable();
-            $table->boolean("necessidades_odontologia")->nullable();
-            $table->boolean("necessidades_oftalmologia")->nullable();
-            $table->boolean("necessidades_psicologia")->nullable();
-            $table->boolean("necessidades_psiquiatria")->nullable();
-            $table->boolean("necessidades_servico_social")->nullable();
-            $table->boolean("necessidades_tratamento_drogas")->nullable();
-            $table->boolean("necessidades_musicoterapia")->nullable();
-            $table->boolean("necessidades_ter_ocupacional")->nullable();
-            $table->boolean("necessidades_pedagogia")->nullable();
+            $table->boolean("necessidades_enfermagem")->default(0)->nullable();
+            $table->boolean("necessidades_medico")->default(0)->nullable();
+            $table->boolean("necessidades_nutricao")->default(0)->nullable();
+            $table->boolean("necessidades_odontologia")->default(0)->nullable();
+            $table->boolean("necessidades_oftalmologia")->default(0)->nullable();
+            $table->boolean("necessidades_psicologia")->default(0)->nullable();
+            $table->boolean("necessidades_psiquiatria")->default(0)->nullable();
+            $table->boolean("necessidades_servico_social")->default(0)->nullable();
+            $table->boolean("necessidades_tratamento_drogas")->default(0)->nullable();
+            $table->boolean("necessidades_musicoterapia")->default(0)->nullable();
+            $table->boolean("necessidades_ter_ocupacional")->default(0)->nullable();
+            $table->boolean("necessidades_pedagogia")->default(0)->nullable();
             $table->string("necessidades_outros")->nullable();
-            $table->boolean("necessidades_completado")->nullable();
+            $table->boolean("necessidades_completado")->default(0)->nullable();
 
-            $table->enum("rededeapoio_equipamentos", ['contato', 'encaminhar'])->nullable();
-            $table->enum("rededeapoio_familia_extensa", ['contato', 'encaminhar'])->nullable();
-            $table->enum("rededeapoio_pais_responsaveis", ['contato', 'encaminhar'])->nullable();
-            $table->enum("rededeapoio_programas_apoio_comunitario", ['contato', 'encaminhar'])->nullable();
-            $table->enum("rededeapoio_programas_protecao", ['contato', 'encaminhar'])->nullable();
-            $table->enum("rededeapoio_servico_atendimento_vitimias_maus_tratos", ['contato', 'encaminhar'])->nullable();
+            $table->enum("rededeapoio_equipamentos", ['contato', 'encaminhar'])->default('encaminhar')->nullable();
+            $table->enum("rededeapoio_familia_extensa", ['contato', 'encaminhar'])->default('encaminhar')->nullable();
+            $table->enum("rededeapoio_pais_responsaveis", ['contato', 'encaminhar'])->default('encaminhar')->nullable();
+            $table->enum("rededeapoio_programas_apoio_comunitario", ['contato', 'encaminhar'])->default('encaminhar')->nullable();
+            $table->enum("rededeapoio_programas_protecao", ['contato', 'encaminhar'])->default('encaminhar')->nullable();
+            $table->enum("rededeapoio_servico_atendimento_vitimias_maus_tratos", ['contato', 'encaminhar'])->default('encaminhar')->nullable();
             $table->string("rededeapoio_outros")->nullable();
-            $table->boolean("rededeapoio_completado")->nullable();
+            $table->boolean("rededeapoio_completado")->default(0)->nullable();
 
-            $table->boolean("religiosidade_atividade_religiosa")->nullable();
-            $table->boolean("religiosidade_assistencia_religiosa")->nullable();
+            $table->boolean("religiosidade_atividade_religiosa")->default(0)->nullable();
+            $table->boolean("religiosidade_assistencia_religiosa")->default(0)->nullable();
             $table->string("religiosidade_denominacao_religiosa")->nullable();
             $table->string("religiosidade_outros")->nullable();
-            $table->boolean("religiosidade_completado")->nullable();
+            $table->boolean("religiosidade_completado")->default(0)->nullable();
 
             $table->string("atividades_escolarizacao")->nullable();
             $table->string("atividades_curso_profissionalizante")->nullable();
@@ -69,7 +69,7 @@ class CreateDadosenecessidadesTable extends Migration
             $table->string("atividades_atividades_culturais")->nullable();
             $table->string("atividades_atividades_esportivas")->nullable();
             $table->string("atividades_outros")->nullable();
-            $table->boolean("atividades_completado")->nullable();
+            $table->boolean("atividades_completado")->default(0)->nullable();
 
             $table->string("tratamentos_medico")->nullable();
             $table->string("tratamentos_nutricao")->nullable();
@@ -80,9 +80,9 @@ class CreateDadosenecessidadesTable extends Migration
             $table->string("tratamentos_uso_drogras")->nullable();
             $table->string("tratamentos_atendimento_hospitalar")->nullable();
             $table->string("tratamentos_outros")->nullable();
-            $table->boolean("tratamentos_completado")->nullable();
+            $table->boolean("tratamentos_completado")->default(0)->nullable();
 
-            $table->boolean("completado")->nullable();
+            $table->boolean("completado")->default(0)->nullable();
 
             $table->softDeletes();
             $table->timestamps();
