f42f06bd4fab3132a03d1f6a0bd0633d22fc50bf
Gabriel Leandro <gabrielleandrojunior@live.com>
2018-05-22 19:37:03 -0300

    Remove Bug Snag

--- config/app.php
+++ config/app.php
@@ -167,7 +167,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
         /*
          * Application Service Providers...
          */
